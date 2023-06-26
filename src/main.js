b503801712370d2dd68961c53301a85abf4199ec
Yuri Cândido <yuri.candido96@hotmail.com>
2023-06-26 10:27:11 -0300

    gerador de senhas

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime'; // Esses 2 import para fazer script rodar em navegadores mais antigos
+
+import formGeraSenha from './modules/formGeraSenha'
+import './assets/css/style.css';
+formGeraSenha();
